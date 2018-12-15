2f7e58308a8e8c419c536182bcd1e9388206e9fb
stronger1234 <stronger1234@live.cn>
2018-12-15 17:15:47 +0800

    try commit

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    std::cout << "xiazhongzhong" << std::endl;
+    std::cout << "xiapengpeng" << std::endl;
 
     return a.exec();
 }
